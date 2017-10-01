cfb94a17e0e513c067dbc0bb048777bdfc06e543
Nahum Bastida <anbr90@hotmail.com>
2017-10-01 00:18:18 -0500

    se agregó el código requerido para cumplir con la tarea de la semana 3 del curso Programación en R de Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Esta función recibe como argumento una matriz y devuelve la matriz inversa indirectamente
+## a partir de lo que se guarda en la memoria cache
 
-## Write a short comment describing this function
+## guarda en la memoria lo necesario para calcular la matriz inversa
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() x
+  setinv<-function(invs) inv<<-invs
+  getinv<- function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
 
 }
 
 
-## Write a short comment describing this function
+## Hace el cálculo de la matriz inversa a partir de la función anterior
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cacheSolve<-function(x){
+    inversa <- x$getinv()
+    if(!is.null(inversa)) {
+      message("getting cached data")
+      return(inversa)
+    }
+    matriz <- x$get()
+    inversa <- solve(matriz)
+    x$setinv(inversa)
+    inversa
+  }
 }
